0df649de3524fa78235a97c58b26b2d24209155b
Gerardusdavid <142146086+Gerardusdavidbayuaji@users.noreply.github.com>
2025-04-30 22:26:13 +0700

    feat: update app

--- backend/app.ts
+++ backend/app.ts
@@ -1,7 +1,16 @@
 import express from "express";
+import cors from "cors";
+
 import router from "./src/routers/polisRouter";
 
 const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
 
 app.use(express.json());
 app.use("/api", router);
